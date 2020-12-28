a10c9914bb9459bf40788f6d0172a3eb9a39a3b1
Emre Kilinc <me@emrekilinc.com>
2020-12-28 15:37:03 +0300

    Change page titles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Denebunu Applications Example.
         </p>
         <a
           className="App-link"
